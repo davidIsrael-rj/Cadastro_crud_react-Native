58bc100b10df9e8743bdb34b924f7729c3ccf6b8
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-24 20:14:55 -0300

    lista de Usuário

---
+++ src/data/users.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email:'joao@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_640.png'
+    },
+    {
+        id: 2,
+        name: 'Carlos Pedro',
+        email:'carlos@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_640.png'
+    },
+    {
+        id: 3,
+        name: 'Maria Julieta',
+        email:'maria@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_640.png'
+    }
+]
