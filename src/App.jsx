2cf61ba93820bae30baca0084bad237d9cc54f32
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-23 21:43:02 -0300

    Componente UserForm e UserList

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,38 @@ import React from "react";
 import 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens'
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import UserList from "./views/UserList";
+import UserForm from "./views/UserForm";
 
 enableScreens();
 
+const Stack = createStackNavigator()
 export default props => {
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-            <Text>
-                Cadastro CRUD
-            </Text>
+            <NavigationContainer>
+                <Stack.Navigator 
+                    initialRouteName="UserList">
+                    <Stack.Screen
+                        name="UserList"
+                        component={UserList}
+                    />
+
+                    <Stack.Screen
+                        name="UserForm"
+                        component={UserForm}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         padding: 20,
         backgroundColor: '#fff'
     }
