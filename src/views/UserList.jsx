a1043a37b983fa2a1212ba361b7bc7f934fc85da
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-25 18:58:06 -0300

    Adicionando Botões editar e delete

--- src/views/UserList.jsx
+++ src/views/UserList.jsx
@@ -1,43 +1,73 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View } from "react-native";
 import users from "../data/users";
-import { Avatar, ListItem } from "@rneui/themed";
+import { Avatar, Button, ListItem } from "@rneui/themed";
 import LinearGradient from "react-native-linear-gradient";
 import TouchableScale from "react-native-touchable-scale";
+import { Icon } from "@rneui/base";
 
 export default props => {
 
+    function confirmaUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim', onPress() {
+                    console.warn('delete ' + user.id + ' - '+user.name)
+                }
+            },
+            { text: 'Não' }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="#FFF" />}
+                />
+                <Button
+                    onPress={() => confirmaUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="#FFF" />}
+                />
+            </>
+        )
+    }
     function getUserItem({ item: user }) {
         return (
-            <ListItem 
-            key={user.id}
-            bottomDivider
-            Component={TouchableScale}
-            friction={90} 
-            tension={100} 
-            activeScale={0.95} 
-            linearGradientProps={{
-              colors: ['#FF9800', '#F44336'],
-              start: { x: 1, y: 0 },
-              end: { x: 0.2, y: 0 },
-            }}
-            ViewComponent={LinearGradient}
-            style={{ paddingTop:5}}
-            onPress={()=> props.navigation.navigate('UserForm')}
+            <ListItem
+
+                key={user.id}
+                bottomDivider
+                Component={TouchableScale}
+                friction={90}
+                tension={100}
+                activeScale={0.95}
+                linearGradientProps={{
+                    colors: ['#FF9800', '#F44336'],
+                    start: { x: 1, y: 0 },
+                    end: { x: 0.2, y: 0 },
+                }}
+                ViewComponent={LinearGradient}
+                style={{ paddingTop: 5 }}
+                onPress={() => props.navigation.navigate('UserForm')}
             >
                 <Avatar
                     rounded
-                    source={{uri:user.avatarUrl }}
+                    source={{ uri: user.avatarUrl }}
                 />
                 <ListItem.Content>
                     <ListItem.Title
-                        style={{color:"white", fontWeight:"bold"}}
+                        style={{ color: "white", fontWeight: "bold" }}
                     >{user.name}</ListItem.Title>
                     <ListItem.Subtitle
-                        style={{color:"white"}}
+                        style={{ color: "white" }}
                     >{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
-                <ListItem.Chevron color="white"/>
+                {/* <ListItem.Chevron color="white" /> */}
+                {getActions(user)}
             </ListItem>
         )
     }
