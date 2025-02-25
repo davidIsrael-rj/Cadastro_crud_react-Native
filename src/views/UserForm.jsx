790e62990cada6087658ca409dbf7f463fb0af9e
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-25 19:37:54 -0300

    Adicionando componentes no formulário

--- src/views/UserForm.jsx
+++ src/views/UserForm.jsx
@@ -1,10 +1,49 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
     return (
 
-        <Text>UserForm  - { user.id}</Text>
-    )
+        <View style={style.form}>
+            <Text>Name</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({ ...user, name })}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({ ...user, email })}
+                placeholder="Informe o Email"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+                placeholder="Informe a Url do Avatar"
+                value={user.avatarUrl}
+            />
+            <Button
+                title='Salvar'
+                onPress={()=>{
+                    navigation.goBack()
+                }}
+            />
+        </View>)
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10
+    }
+})
